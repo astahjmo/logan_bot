1b685775b576ed6fbaf457cde92e2644bb358962
Johnatan <contato.johnatan373@gmail.com>
2021-12-06 13:42:35 -0300

    Fix suggestion and ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,38 @@
+const Discord = require("discord.js")
+
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-      console.log(`[API] ${client.user.username} está no server ${client.guilds.cache.size} on`);
+      //let channel = client.channels.cache.get("902330008155070495")
+      //channel.join()
+      let canal = client.channels.cache.get("902326610055823430")
+            const acordei = new Discord.MessageEmbed()
+            .setTitle(`> Made by Sr.Luzifer`)
+            .setDescription(` Inciciando novamente !!!`)
+             .setTimestamp()
+            .setColor('PURPLE')
+            setStatus();
+            setInterval(() => setStatus(), 5000)
+            canal.send({embeds: [acordei]})
+
+            function setStatus() { 
+              var tabela = [ 
+                {name: `Cuido de ${client.users.cache.size} usuários!`, type: 'PLAYING'}, 
+                {name: `Fui feito pelo Sr.Luzifer#0666 e Astaroth#0373!`, type: 'COMPETING'}, 
+                {name: `Estou a gerenciar  ${client.channels.cache.size} canais!`, type: 'WATCHING'},
+                {name:` Filho do Logan :D`}, 
+                {name: `+help`, type: 'STREAMING', url: 'https://www.twitch.tv/masterleahsakurai'},
+                 {name: ` Filho do Logan :D`, type: 'STREAMING', url: 'https://www.twitch.tv/masterleahsakurai'}
+              
+              ];
+    
+              var altstatus = tabela[Math.floor(Math.random() * tabela.length)]
+              client.user.setActivity(altstatus) }
+            setInterval(() => setStatus(), 5000)
     }
+    
 }
+
+
+
--- src/events/suggestion.js
+++ src/events/suggestion.js
@@ -5,24 +5,19 @@ module.exports = {
     name: "messageCreate",
 
     async execute(client,message) {
-        // verificar se a mensagem foi mandanda no canal de sugestão
         if (message.channel.id != guild.suggestion_channel) return;
-        // verificar se não foi um bot que mandou
         if (message.author.bot) return;
-        // isso aqui pega toda a mensagem enviada
         let args = message.content
-        // isso deleta a mensagem que o cara enviou
         message.delete()
 
-        // isso cria um embed
         const embed = new Discord.MessageEmbed()
-        .setAuthor(message.author.username,message.author.avatarURL())
+        .setAuthor(`Sugestão de: ${message.author.username}`,message.guild.iconURL())
         .setColor("00fff7")
         .setThumbnail(message.author.avatarURL())
         .setDescription(`> **${args}**`)
         .setFooter(`Para sugerir algo basta escreveres aqui !!`)
         .setTimestamp()
-        //isso aqui manda o embed
+        
         message.channel.send({embeds: [embed]}).then(msg =>{
             msg.react("👍")
             msg.react("👎")
